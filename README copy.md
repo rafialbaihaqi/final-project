cba86713124c0d5b2d3c9c6000f3c741f1773e3f
rafialbaihaqi <72422140+rafialbaihaqi@users.noreply.github.com>
2022-06-14 18:55:24 +0700

    upload file backend

---
+++ README copy.md
@@ -0,0 +1,1 @@
+Ini repository final project 
---
+++ repositories/userRepository.js
@@ -0,0 +1,68 @@
+const {
+    users
+} = require("../models");
+
+class usersRepository {
+    static async getByEmail({
+        email
+    }) {
+        const getUser = await users.findOne({
+            where: {
+                email: email
+            }
+        });
+
+        return getUser;
+    }
+
+    static async getById({
+        id
+    }) {
+        const getUser = await users.findOne({
+            where: {
+                id
+            }
+        });
+
+        return getUser;
+    }
+
+    static async create({
+        name,
+        email,
+        password,
+    }) {
+        const createdUser = users.create({
+            name,
+            email,
+            password,
+        });
+
+        return createdUser;
+    }
+
+    static async updateById({
+        id,
+        name,
+        kota,
+        alamat,
+        noHp,
+        image
+    }) {
+        const updateById = await users.update({
+            name,
+            kota,
+            alamat,
+            noHp,
+            image
+        }, {
+            where: {
+                id
+            }
+        });
+
+        return updateById;
+    }
+}
+
+module.exports = usersRepository;
